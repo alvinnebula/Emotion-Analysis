9977931fb580ce60ea5f1ed339e59808aabfb47f
Alvin <yiwen.chen@vanderbilt.edu>
2023-04-26 00:48:43 -0500

    update

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from mtcnn.mtcnn import MTCNN
 import base64
 
 
-model = load_learner("models/resnet-50.pkl")
+model = load_learner("resnet-50.pkl")
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_classifier = cv2.CascadeClassifier (cv2.data.haarcascades + 'haarcascade_eye_tree_eyeglasses.xml')
 mouth_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
